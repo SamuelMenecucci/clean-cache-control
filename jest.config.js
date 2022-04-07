d3c8ae8ad3850ebc0aff9324532048bb38de6e0f
Samuel Menecucci Moura <samuelmm@ciandt.com>
2022-04-07 10:33:25 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+//arquivo de configuração do jest, ferramenta de teste para javascript
+module.exports = {
+  roots: ["<rootDir>/src"],
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir/src/$1",
+  },
+};
