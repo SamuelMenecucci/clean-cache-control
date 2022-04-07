3e87752e44652d5196d6d1d0ae604cb47e4eff3a
Samuel Menecucci Moura <samuelmm@ciandt.com>
2022-04-07 12:06:02 -0300

    ensure LocalSavePurchases does not delete cache upon creation

---
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -0,0 +1,17 @@
+class LocalSavePurchases {
+  constructor(private readonly cacheStore: CacheStore) {}
+}
+
+interface CacheStore {}
+
+class CacheStoreSpy implements CacheStore {
+  deleteCallsCount = 0;
+}
+
+describe("LocaSavePurchases", () => {
+  test("Should not delete cache on sut.init", () => {
+    const cacheStore = new CacheStoreSpy();
+    new LocalSavePurchases(cacheStore);
+    expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+});
