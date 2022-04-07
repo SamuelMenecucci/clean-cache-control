45be75ed976ca9b5b2c08cda8eb0ac3264115ec9
Samuel Menecucci Moura <samuelmm@ciandt.com>
2022-04-07 12:13:22 -0300

    ensure LocalSavePurchases deletes old cache on save

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,11 +1,21 @@
 class LocalSavePurchases {
   constructor(private readonly cacheStore: CacheStore) {}
+
+  async save(): Promise<void> {
+    this.cacheStore.delete();
+  }
 }
 
-interface CacheStore {}
+interface CacheStore {
+  delete: () => void;
+}
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0;
+
+  delete(): void {
+    this.deleteCallsCount++;
+  }
 }
 
 describe("LocaSavePurchases", () => {
@@ -13,5 +23,14 @@ describe("LocaSavePurchases", () => {
     const cacheStore = new CacheStoreSpy();
     new LocalSavePurchases(cacheStore);
     expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+});
+
+describe("LocaSavePurchases", () => {
+  test("Should delete old cache on sut.init", async () => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalSavePurchases(cacheStore);
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
   });
 });
