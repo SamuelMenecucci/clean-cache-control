adff839ff8ab2effad949c4c7af32f838daffc1b
Samuel Menecucci Moura <samuelmm@ciandt.com>
2022-04-10 15:27:07 -0300

    merge tests

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -18,16 +18,6 @@ describe("LocaLSavePurchases", () => {
     expect(cacheStore.messages).toEqual([]);
   });
 
-  test("Should delete old cache on sut.save", async () => {
-    const { cacheStore, sut } = makeSut();
-    await sut.save(mockPurchases());
-    expect(cacheStore.messages).toEqual([
-      CacheStoreSpy.Message.delete,
-      CacheStoreSpy.Message.insert,
-    ]);
-    expect(cacheStore.deleteKey).toBe("purchases");
-  });
-
   test("Should not insert new Cache if delete fails", async () => {
     const { cacheStore, sut } = makeSut();
     cacheStore.simulateDeleteError();
@@ -44,6 +34,7 @@ describe("LocaLSavePurchases", () => {
       CacheStoreSpy.Message.delete,
       CacheStoreSpy.Message.insert,
     ]);
+    expect(cacheStore.deleteKey).toBe("purchases");
     expect(cacheStore.insertKey).toBe("purchases");
     expect(cacheStore.insertValues).toEqual(purchases);
   });
