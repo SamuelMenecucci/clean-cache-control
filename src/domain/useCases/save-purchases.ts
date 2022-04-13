3d9d5ad7c9f3a3c3b013a3b1a583206be3bed56a
Samuel Menecucci Moura <samuelmm@ciandt.com>
2022-04-13 07:42:33 -0300

    add LoadPurchases usecase

--- src/domain/useCases/save-purchases.ts
+++ src/domain/useCases/save-purchases.ts
@@ -1,3 +1,5 @@
+import { PurchaseModel } from "@/domain/models";
+
 //arquivo entidades e interfaces
 export interface SavePurchases {
   //tipo uma função chamada save, que recebe como paramêtro purchases, que eu tipo como um array do tipo Params, e essa função não retorna nada. deixo como promise para que eu consiga trabalhar de forma assíncrona
@@ -5,11 +7,7 @@ export interface SavePurchases {
 }
 
 export namespace SavePurchases {
-  export type Params = {
-    id: string;
-    date: Date;
-    value: number;
-  };
+  export type Params = PurchaseModel;
 }
 
 // //como os dois estão diretamente ligados, faria mais sentido deixar dessa forma, mas o typescript não permite assim, por isso iremos utilizar um namespace para que não precisemos fazer dois imports quando formos utilizar  a interface
