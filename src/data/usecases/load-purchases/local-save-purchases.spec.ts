02c939024c25bff70d3807eb75703aa858353abd
Samuel Menecucci Moura <samuelmm@ciandt.com>
2022-04-13 07:47:49 -0300

    rename class

--- src/data/usecases/load-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-save-purchases.spec.ts
@@ -1,19 +1,19 @@
-import { LocalSavePurchases } from "@/data/usecases";
+import { LocalLoadPurchases } from "@/data/usecases";
 import { mockPurchases, CacheStoreSpy } from "@/data/tests";
 import { timeStamp } from "console";
 
 type SutTypes = {
-  sut: LocalSavePurchases;
+  sut: LocalLoadPurchases;
   cacheStore: CacheStoreSpy;
 };
 
 const makeSut = (timestamp = new Date()): SutTypes => {
   const cacheStore = new CacheStoreSpy();
-  const sut = new LocalSavePurchases(cacheStore, timestamp);
+  const sut = new LocalLoadPurchases(cacheStore, timestamp);
   return { sut, cacheStore };
 };
 
-describe("LocaLSavePurchases", () => {
+describe("LocaLLoadPurchases", () => {
   test("Should not delete or insert cache on sut.init", () => {
     const { cacheStore } = makeSut();
     expect(cacheStore.actions).toEqual([]);
