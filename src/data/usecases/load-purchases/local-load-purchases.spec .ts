a268edccf0393360a1735f50bb20a29c8f6b0934
Samuel Menecucci Moura <samuelmm@ciandt.com>
2022-04-13 07:51:16 -0300

    ensure LocalLoadPurchases will not delete cache upon creation

---
+++ src/data/usecases/load-purchases/local-load-purchases.spec .ts
@@ -0,0 +1,21 @@
+import { LocalLoadPurchases } from "@/data/usecases";
+import { mockPurchases, CacheStoreSpy } from "@/data/tests";
+import { timeStamp } from "console";
+
+type SutTypes = {
+  sut: LocalLoadPurchases;
+  cacheStore: CacheStoreSpy;
+};
+
+const makeSut = (timestamp = new Date()): SutTypes => {
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalLoadPurchases(cacheStore, timestamp);
+  return { sut, cacheStore };
+};
+
+describe("LocaLLoadPurchases", () => {
+  test("Should not delete or insert cache on sut.init", () => {
+    const { cacheStore } = makeSut();
+    expect(cacheStore.actions).toEqual([]);
+  });
+});
