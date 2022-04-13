7b9509f9580c7f428e79508a22093140ed100a40
Samuel Menecucci Moura <samuelmm@ciandt.com>
2022-04-13 09:10:44 -0300

    rename var to give more context

--- src/data/usecases/load-purchases/local-load-purchases.ts
+++ src/data/usecases/load-purchases/local-load-purchases.ts
@@ -5,12 +5,12 @@ export class LocalLoadPurchases implements SavePurchases, LoadPurchases {
   private readonly key = "purchases";
   constructor(
     private readonly cacheStore: CacheStore,
-    private readonly timestamp: Date
+    private readonly currentDate: Date
   ) {}
 
   async save(purchases: Array<SavePurchases.Params>): Promise<void> {
     this.cacheStore.replace(this.key, {
-      timestamp: this.timestamp,
+      timestamp: this.currentDate,
       value: purchases,
     });
   }
